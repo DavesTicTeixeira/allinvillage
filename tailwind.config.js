3761ed208e7645d215c5d8ea8024477efdd83113
David Teixeira <88932109+DavesTicTeixeira@users.noreply.github.com>
2022-11-18 23:09:04 +0000

    teste_01

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   theme:{
-    backgroundImage: {
-      'hero': "url('../public/images/hero.jpg')",
     },
-},
   variants: {},
   plugins: [
     // require('@tailwindcss/custom-forms'),
